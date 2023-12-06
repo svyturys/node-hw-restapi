d497ec8b0c598e650b27c9d55ca54d5afd34bee7
svyturys <tarento.sv@gmail.com>
2023-12-06 22:57:42 +0200

    res

--- controllers/users/register.js
+++ controllers/users/register.js
@@ -1,7 +1,7 @@
 const { User } = require("../../models");
 
 const { HttpError } = require("../../helpers");
-
+const gravatar = require("gravatar");
 const bcrypt = require("bcrypt");
 
 const register = async (req, res) => {
@@ -11,7 +11,12 @@ const register = async (req, res) => {
     throw HttpError(409, "Email is already in use");
   }
   const hashPassword = await bcrypt.hash(password, 10);
-  const newUser = await User.create({ ...req.body, password: hashPassword });
+  const avatarURL = gravatar.url(email);
+  const newUser = await User.create({ 
+    ...req.body, 
+    password: hashPassword,
+  avatarURL,
+ });
 
   res.status(201).json({
     user: {
